ac377f89a3ac5802189640c69563377a9325e540
vinimatheus <111582437+vinimatheus@users.noreply.github.com>
2025-01-06 11:21:29 -0300

    email correção

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,7 +1,7 @@
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const emailFrom = process.env.EMAIL_FROM;
+const emailFrom = process.env.EMAIL_FROM || 'default@example.com';
 const domain = process.env.NEXTAUTH_URL;
 
 /**
